b6ff8b985883cb9f5ff15c5f968c131a76157afd
Kielan <ki.lemons@gmail.com>
2026-03-22 16:51:22 -0600

    Create SelectionManager.ts

---
+++ SelectionManager.ts
@@ -0,0 +1,45 @@
+import * as THREE from "three"
+
+export class SelectionManager {
+  raycaster = new THREE.Raycaster()
+  mouse = new THREE.Vector2()
+  camera: THREE.Camera
+  scene: THREE.Scene
+  selected: any = null
+  constructor(camera: THREE.Camera, scene: THREE.Scene) {
+    this.camera = camera
+    this.scene = scene
+  }
+  pick(event: MouseEvent) {
+    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+    this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+    this.raycaster.setFromCamera(this.mouse, this.camera)
+    const hits = this.raycaster.intersectObjects(
+      this.scene.children,
+      true
+    )
+    if (hits.length === 0) return null
+    const obj = hits[0].object
+    // 🔥 detect pad
+    if (obj.userData.pad) {
+      this.select(obj.userData.pad)
+      return obj.userData.pad
+    }
+    return null
+  }
+  select(obj: any) {
+    if (this.selected) {
+      this.clearSelection(this.selected)
+    }
+    this.selected = obj
+    // highlight
+    if (obj.mesh) {
+      obj.mesh.material.emissive.set(0x00ff00)
+    }
+  }
+  clearSelection(obj: any) {
+    if (obj.mesh) {
+      obj.mesh.material.emissive.set(0x000000)
+    }
+  }
+}
